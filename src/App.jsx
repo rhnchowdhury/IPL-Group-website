f9d3da4496febcbd5af60cc3c4c785a0abe8c379
rhnchowdhury <rhnchdri@gmail.com>
2024-02-11 15:02:17 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="mx-5">
+        <h1 className="py-5">Lorem ipsum dolor sit amet consectetur.</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
