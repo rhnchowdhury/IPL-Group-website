0a54d8051a08db1290cc280b0a4800fb5a4b4b6e
rhnchowdhury <rhnchdri@gmail.com>
2024-02-15 16:38:42 +0600

    sec part added

--- src/Components/FirstPart/FirstPart.jsx
+++ src/Components/FirstPart/FirstPart.jsx
@@ -1,14 +1,16 @@
+import img from '../../assets/sir.jpg';
 
 const FirstPart = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+            <div className="hero min-h-screen bg-[#E8E4D8]">
+                <div className="hero-content mx-20 flex-col lg:flex-row-reverse">
+                    <img src={img} className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className=" font-medium">We&apos;re awessome</h1>
+                        <p className='font-bold text-xl'>What We Are Done Good</p>
+                        <p className="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repudiandae earum quo odio minus atque corporis? Placeat porro eaque dicta, nam ea distinctio enim nostrum ex atque sint <br /> <br />saepe iusto quae? Asperiores sunt aspernatur, odio dolorem possimus excepturi vero sapiente mollitia fuga obcaecati laborum quam omnis repudiandae ea iure perferendis.</p>
+                        <button className="btn ">Get Started</button>
                     </div>
                 </div>
             </div>
