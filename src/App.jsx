df1080485914cd5a61f6ddcbf1443d374222412d
rhnchowdhury <rhnchdri@gmail.com>
2024-02-17 18:24:19 +0600

    services sect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import FirstPart from "./Components/FirstPart/FirstPart"
+import FirstPart from "./Components/Others/FirstPart"
 import Navbar from "./Components/Navbar/Navbar"
+import Services from "./Components/Others/Services"
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <FirstPart></FirstPart>
+      <Services></Services>
     </div>
   )
 }
